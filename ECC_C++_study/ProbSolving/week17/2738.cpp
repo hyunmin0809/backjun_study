a34ee6cd786914f3604c1f180b0168f71636ec1e
hyunmin <khm010809@naver.com>
2024-08-02 23:21:07 +0900

    add: 2738.cpp

---
+++ ECC_C++_study/ProbSolving/week17/2738.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+unsigned long long fibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    unsigned long long a = 0, b = 1, c;
+    for (int i = 2; i <= n; ++i) {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main() {
+    int n;
+    cin >> n; 
+    cout << fibonacci(n) << endl; 
+    return 0;
+}s
