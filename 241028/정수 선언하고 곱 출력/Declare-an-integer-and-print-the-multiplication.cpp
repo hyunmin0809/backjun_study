d264fff5275c03ce8f1e25eecaeac1859b12442c
Hyunmin Kim <90595020+hyunmin0809@users.noreply.github.com>
2024-10-28 10:45:38 +0900

    Title: 정수 선언하고 곱 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241028/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
+++ 241028/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <string>
+
 using namespace std;
 
 int main() {
+    // 변수 선언
     int a = 26, b = 5;
-
-    cout << to_string(a) + " * " + to_string(b) + " = " + to_string(a*b);
+    
+    // 출력
+    cout << a << " * " << b << " = " << a * b;
     return 0;
 }
