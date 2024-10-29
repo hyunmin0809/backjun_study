1c8464e94c82ca59dd59fa65ea346e424e8c690c
Hyunmin Kim <90595020+hyunmin0809@users.noreply.github.com>
2024-10-29 13:32:37 +0900

    Title: 데이터 교환, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241029/데이터 교환/exchange-data.cpp
+++ 241029/데이터 교환/exchange-data.cpp
@@ -3,13 +3,11 @@ using namespace std;
 
 int main() {
     int a = 5, b = 6, c =7;
-    int temp1, temp2;
-    temp1 = b;
-    b = a;
-    temp2 = c;
-    c = temp1;
-    a = temp2;
-
+   	int temp = a;
+	a = c;
+    c = b;
+	b = temp;
+    
     cout << a << endl;
     cout << b << endl;
     cout << c << endl;
