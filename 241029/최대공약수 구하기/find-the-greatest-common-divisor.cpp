4f4cf6162152ff8384196eb21185e89338b776cb
Hyunmin Kim <90595020+hyunmin0809@users.noreply.github.com>
2024-10-29 14:23:32 +0900

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241029/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 241029/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> n >> m;
     int temp = n > m ? m : n; //temp에 작은거
 
-    for(int i = 1; i <= temp/2; i++){
+    for(int i = 1; i <= temp; i++){
         if(n % i == 0 && m % i == 0){
             result = i;
         }
