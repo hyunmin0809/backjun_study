9ddddb5436c842a815d9796445be8f16fdaa4ad1
Hyunmin Kim <90595020+hyunmin0809@users.noreply.github.com>
2024-11-04 09:51:26 +0900

    Title: 함수를 이용한 369 게임, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241104/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+bool isThree(int n){
+    return n % 3 == 0;
+}
+
+bool doHaveThree(int n){ 
+    while(n > 0){
+        if((n%10)%3 == 0){ 
+            return true;
+        }
+        n /= 10;
+    }
+    return false;
+}
+
+bool isMagicNum(int n){
+    return doHaveThree(n) || isThree(n);
+}
+
+
+int main() {
+    int a, b, cnt = 0;
+    cin >> a >> b;
+    for(int i = a; i <= b; i++){
+        if(isMagicNum(i)){
+            cnt++; 
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
