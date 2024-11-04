3fcd5d2442346f37b3bb12b7a8d708c10da4bd22
Hyunmin Kim <90595020+hyunmin0809@users.noreply.github.com>
2024-11-04 10:09:30 +0900

    Title: 함수를 이용한 소수 판별, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241104/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 241104/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 bool isPrime(int n){
     for(int i = 2; i < n; i++){
-        if(n% i == 0) return false; 
+        if(n%i == 0) return false; 
     }
     return true;
 }
@@ -12,7 +12,7 @@ int main() {
     int a, b, sum = 0;
     cin >> a >> b;
     for(int i = a; i <= b; i++){
-        if(isPrime(i)){
+        if(isPrime(i) &&  i != 1){
             sum += i;
         }
     }
