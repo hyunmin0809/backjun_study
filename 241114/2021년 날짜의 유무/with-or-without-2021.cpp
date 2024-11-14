e4654baa9b224fe2b3e8cf6f9f381855f56d6cfa
Hyunmin Kim <90595020+hyunmin0809@users.noreply.github.com>
2024-11-14 23:13:19 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241114/2021년 날짜의 유무/with-or-without-2021.cpp
+++ 241114/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -4,7 +4,8 @@ using namespace std;
 string sol(int M, int D){
     switch(M){
         case 1: case 3: case 5: case 7: case 8: case 10: case 12: if(D <= 31) return "Yes";
-        case 2: case 4: case 6: case 9: case 11: if(D<=30) return "Yes";
+        case 4: case 6: case 9: case 11: if(D<=30) return "Yes";
+        case 2: if(D<=28) return "Yes";
         default: return "No";
     }
 
