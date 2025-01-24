fe821a126e5b64df0f8fba45139f5be8f915590f
Hyunmin Kim <90595020+hyunmin0809@users.noreply.github.com>
2025-01-24 13:21:46 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 1090ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 250124/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int f(int cnt, int max){
+    if(cnt == 0) return max;
+
+    if(arr[cnt-1] > max){
+        max = arr[cnt-1];
+    }
+    return f(cnt--, max);
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    cout << f(n, 0);
+
+    return 0;
+}
